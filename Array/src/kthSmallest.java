db7c59d5d570e2a956fbbc84d59107f587ec2d7e
Anand_Mishra <mishraanand158@gmail.com>
2022-09-22 00:03:42 +0530

    MaxMinArrayMinComaparison.java

--- Array/src/kthSmallest.java
+++ Array/src/kthSmallest.java
@@ -1,5 +1,6 @@
 import java.io.PrintWriter;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
@@ -27,7 +28,7 @@ public class kthSmallest {
 class Solution {
     public static int kthSmallest(int[] arr, int l, int r, int k) {
         //default priorityQueue is  min priorityQueue
-        PriorityQueue<Integer> maxHeap = new PriorityQueue(k + 1, Collections.reverseOrder());
+        PriorityQueue<Integer> maxHeap = new PriorityQueue(new CustomComparator());
 
         for (int i = l; i <= r; i++) {
             maxHeap.add(arr[i]);  //0 1 2 3 k =3
@@ -40,4 +41,13 @@ class Solution {
 
 }
 
+class CustomComparator implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return -(o1 - o2);   //making false if current value is
+        // in 2nd loop  # o1 will be 10 and o2 will be 7 res = -3
+        //              #o1 =4 and o2 :10 res = 6 positive
+        // o1:20 and o2:7   and then , ## o1 : 20 and o2 : 10 ##
+    }
+}
 
