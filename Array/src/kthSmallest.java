beda61c35c458caf6b838ca29ba61392599c39fc
Anand_Mishra <mishraanand158@gmail.com>
2022-09-21 16:58:27 +0530

    change

--- Array/src/kthSmallest.java
+++ Array/src/kthSmallest.java
@@ -1,36 +1,39 @@
 import java.io.PrintWriter;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class kthSmallest {
-
-
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         PrintWriter out = new PrintWriter(System.out);
         int t = sc.nextInt();
 
         while (t-- > 0) {
-            int n = sc.nextInt();
-            int arr[] = new int[n];
-
+            int n = sc.nextInt(); //N = 5
+            int arr[] = new int[n]; //arr[] = 7 10 4 20 15
             for (int i = 0; i < n; i++)
                 arr[i] = sc.nextInt();
 
-            int k = sc.nextInt();
+            int k = sc.nextInt(); // K = 4
             Solution ob = new Solution();
-            out.println(ob.kthSmallest(arr, 0, n - 1, k));
+            out.println(ob.kthSmallest(arr, 0, n - 1, k)); // Output : 15
         }
         out.flush();
     }
-
-
 }
 
 
 class Solution {
     public static int kthSmallest(int[] arr, int l, int r, int k) {
-        //Your code here
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue(k);
+        for(int i=l;i<k;i++)
+            priorityQueue.add(arr[l]);
+
+        for (int j= k;j<r;j++){
+
+        }
+
 
         return 1;
     }
