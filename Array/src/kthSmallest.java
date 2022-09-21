b63abbf5846bec074fe49aeee82784fc0ba2f33d
Anand_Mishra <mishraanand158@gmail.com>
2022-09-21 23:15:51 +0530

    Kth min element

--- Array/src/kthSmallest.java
+++ Array/src/kthSmallest.java
@@ -1,4 +1,5 @@
 import java.io.PrintWriter;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
@@ -10,7 +11,8 @@ public class kthSmallest {
 
         while (t-- > 0) {
             int n = sc.nextInt(); //N = 5
-            int arr[] = new int[n]; //arr[] = 7 10 4 20 15
+            int arr[] = new int[n]; //arr[] = 7 10 4 20 15      10 , 7,4     20>10 leave , 10 ,7 ,4 then 15>peek leave
+
             for (int i = 0; i < n; i++)
                 arr[i] = sc.nextInt();
 
@@ -22,20 +24,24 @@ public class kthSmallest {
     }
 }
 
-
 class Solution {
     public static int kthSmallest(int[] arr, int l, int r, int k) {
+        //default priorityQueue is  min priorityQueue
+        PriorityQueue<Integer> maxHeap = new PriorityQueue(k, Collections.reverseOrder());
 
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue(k);
-        for(int i=l;i<k;i++)
-            priorityQueue.add(arr[l]);
+        for (int i = l; i < k; i++)
+            maxHeap.add(arr[i]);  //0 1 2 3 k =3
 
-        for (int j= k;j<r;j++){
+        int j = k;
+        for (; j <= r; j++) {
+            int peekValue = maxHeap.peek();
 
+            if (arr[j] < peekValue) {
+                maxHeap.poll();
+                maxHeap.add(arr[k]);
+            }
         }
-
-
-        return 1;
+        return maxHeap.peek();
     }
 }
 
