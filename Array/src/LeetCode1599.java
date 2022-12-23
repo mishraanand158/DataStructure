2a4e829711a144d0aa9ba114831b569da4546747
Anand <mishraanand158@gmail.com>
2022-12-23 22:07:04 +0530

    leetcode1593 code added

---
+++ Array/src/LeetCode1599.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+
+public class LeetCode1599 {
+
+    public static void main(String[] args) {
+        Solution s1 = new Solution();
+        String s = "wwwzfvedwfvhsww";
+        // System.out.println(s1.minOperationsMaxProfit(s));
+        //s1.maxUniqueSplit(s);
+
+
+    }
+
+    static class Solution {
+        public int minOperationsMaxProfit(int[] customers, int boardingCost, int runningCost) {   //www zfvedwfvhs ww  ans 11 : w,ww,z,f,v,e,d,wf,vh,s
+            return 0;
+
+        }
+    }
+}
