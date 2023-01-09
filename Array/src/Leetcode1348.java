03455343a0b53c5fd31101b3af6ca3fa003fb9fb
Anand <mishraanand158@gmail.com>
2023-01-09 20:46:16 +0530

    1348. Tweet Count Frequency

--- Array/src/Leetcode1348.java
+++ Array/src/Leetcode1348.java
@@ -29,10 +29,10 @@ public class Leetcode1348 {
     public static void main(String[] args) {
 
     }
-    Map<String, List<Integer>> map = new HashMap<>();
+    Map<String, List<Integer>> map;
 
     public Leetcode1348() {
-
+        map = new HashMap<>();
     }
 
     public void recordTweet(String tweetName, int time) {
@@ -51,8 +51,36 @@ public class Leetcode1348 {
 
         List<Integer> tweetTimeList = map.get(tweetName);
         Collections.sort(tweetTimeList);
+        //"minute", "hour", or "day"
+        int duration = 0;
+        if ("minute".equals(freq)) {
+            duration = 60;
 
-        for (int val : tweetTimeList) {
+        } else if ("hour".equals(freq)) {
+            duration = 60 * 60;
+        } else if ("day".equals(freq)) {
+            duration = 60 * 60 * 24;
+        }
+
+        int start_time = startTime;
+
+        int end_time = Math.min(start_time + duration - 1, endTime);
+
+
+        for (int iter = 0; start_time <= endTime; ) {    //0,60       0,59 ,60,60
+            //0,10,60
+            while (iter < tweetTimeList.size() && tweetTimeList.get(iter) < start_time) {
+                iter++;
+            }
+            int count = 0;
+            while (iter < tweetTimeList.size() && tweetTimeList.get(iter) <= end_time) {
+                count++;
+                iter++;
+            }
+
+            tweetList.add(count);
+            start_time = end_time + 1;
+            end_time = Math.min(start_time + duration + 1, end_time);
 
         }
 
