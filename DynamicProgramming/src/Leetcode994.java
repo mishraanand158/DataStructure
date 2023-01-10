9589e87a2d252433a874752f0b13510434fb9788
Anand <mishraanand158@gmail.com>
2023-01-10 09:46:57 +0530

    994. orangesRotting

--- DynamicProgramming/src/Leetcode994.java
+++ DynamicProgramming/src/Leetcode994.java
@@ -27,18 +27,24 @@ class Solution994 {
     public int orangesRotting(int[][] grid) {
         Queue<Pair> queue = new ArrayDeque<>();
 
+        boolean check = false;
+        int val = 0;
         for (int i = 0; i < grid.length; i++)
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 2) {
                     Pair pair = new Pair(i, j);
                     queue.add(pair);
-                }
-
+                } else if (grid[i][j] == 1)
+                    val++;
             }
 
-        int count = 0;
+        if (val == 0)
+            return 0;
+
+        int count = -1;
         while (!queue.isEmpty()) {
             int queueSize = queue.size();
+
             for (int i = 0; i < queueSize; i++) {
 
                 Pair result = queue.remove();
@@ -46,26 +52,40 @@ class Solution994 {
                 int row = result.row;
                 int col = result.col;
 
-                if (row - 1 >= 0 && col - 1 >= 0 && grid[row - 1][col - 1] == 1) {
-                    grid[row - 1][col - 1] = 2;
-                    queue.add(new Pair(row - 1, col - 1));
+                if (row - 1 >= 0 && col >= 0 && grid[row - 1][col] == 1) {
+                    grid[row - 1][col] = 2;
+                    val--;
+                    ////System.out.println((row - 1) + " " + col);
+                    queue.add(new Pair(row - 1, col));
                 }
-                if (row - 1 >= 0 && col + 1 < grid[0].length && grid[row - 1][col + 1] == 1) {
-                    grid[row - 1][col + 1] = 2;
-                    queue.add(new Pair(row - 1, col + 1));
+                if (row >= 0 && col + 1 < grid[0].length && grid[row][col + 1] == 1) {
+                    grid[row][col + 1] = 2;
+                    val--;
+                    //System.out.println(row + " " + (col + 1));
+                    queue.add(new Pair(row, col + 1));
                 }
-                if (row + 1 < grid.length && col - 1 >= 0 && grid[row + 1][col - 1] == 1) {
-                    grid[row + 1][col - 1] = 2;
-                    queue.add(new Pair(row + 1, col - 1));
+                if (row + 1 < grid.length && col >= 0 && grid[row + 1][col] == 1) {
+                    grid[row + 1][col] = 2;
+                    val--;
+                    //System.out.println((row + 1) + " " + col);
+                    queue.add(new Pair(row + 1, col));
                 }
-                if (row + 1 < grid.length && col + 1 < grid[0].length && grid[row + 1][col + 1] == 1) {
-                    grid[row + 1][col + 1] = 2;
-                    queue.add(new Pair(row + 1, col + 1));
+                if (row >= 0 && col - 1 >= 0 && grid[row][col - 1] == 1) {
+                    grid[row][col - 1] = 2;
+                    val--;
+                    //System.out.println(row + " " + (col - 1));
+                    queue.add(new Pair(row, col - 1));
                 }
+
+                //    //System.out.println("********************");
             }
+
+            //System.out.println("#############");
             count++;
         }
-        System.out.println(count);
+        if (val != 0)
+            return -1;
+        //System.out.println(count);
         return count;
     }
 }
