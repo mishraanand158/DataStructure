7e39c219a2bd032b04c80e117466cc9a8049fcbe
Anand Mishra <mishraanand158@gmail.com>
2024-02-19 15:54:40 +0530

    Time: 1 ms (99.99%), Space: 42.4 MB (53.35%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,77 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        Queue<Pair> queue = new ArrayDeque<>();
+
+        boolean check = false;
+        int val = 0;
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 2) {
+                    Pair pair = new Pair(i, j);
+                    queue.add(pair);
+                } else if (grid[i][j] == 1)
+                    val++;
+            }
+
+        if (val == 0)
+            return 0;
+
+        int count = -1;
+        while (!queue.isEmpty()) {
+            int queueSize = queue.size();
+
+            for (int i = 0; i < queueSize; i++) {
+
+                Pair result = queue.remove();
+
+                int row = result.row;
+                int col = result.col;
+
+                if (row - 1 >= 0 && col >= 0 && grid[row - 1][col] == 1) {
+                    grid[row - 1][col] = 2;
+                    val--;
+                    ////System.out.println((row - 1) + " " + col);
+                    queue.add(new Pair(row - 1, col));
+                }
+                if (row >= 0 && col + 1 < grid[0].length && grid[row][col + 1] == 1) {
+                    grid[row][col + 1] = 2;
+                    val--;
+                    //System.out.println(row + " " + (col + 1));
+                    queue.add(new Pair(row, col + 1));
+                }
+                if (row + 1 < grid.length && col >= 0 && grid[row + 1][col] == 1) {
+                    grid[row + 1][col] = 2;
+                    val--;
+                    //System.out.println((row + 1) + " " + col);
+                    queue.add(new Pair(row + 1, col));
+                }
+                if (row >= 0 && col - 1 >= 0 && grid[row][col - 1] == 1) {
+                    grid[row][col - 1] = 2;
+                    val--;
+                    //System.out.println(row + " " + (col - 1));
+                    queue.add(new Pair(row, col - 1));
+                }
+
+                //    //System.out.println("********************");
+            }
+
+            //System.out.println("#############");
+            count++;
+        }
+        if (val != 0)
+            return -1;
+        //System.out.println(count);
+        return count;
+    }
+}
+
+
+class Pair {
+    int row;
+    int col;
+
+    public Pair(int row1, int col1) {
+        this.row = row1;
+        this.col = col1;
+    }
+}
