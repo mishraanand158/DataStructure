2bcc07cae2c2ee5f6ead9446bdf2bfbb8ab6dd60
Anand Mishra <mishraanand158@gmail.com>
2024-04-13 22:38:29 +0530

    Time: 4 ms (56.64%), Space: 43.3 MB (53.43%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -18,7 +18,7 @@ class Solution {
                                               start++;
                                           } else if(!Character.isLetterOrDigit(s.charAt(end)))
                                                    end --;
-                                                   else if(s.charAt(start) != s.charAt(end)){
+                                                   else if(checkNotEquals(Character.toLowerCase(s.charAt(start)),Character.toLowerCase(s.charAt(end)))) {
                                                        return false;
                                                    }
                                                    else {
@@ -33,4 +33,13 @@ class Solution {
         return res;
     
     }
+    
+    public boolean checkNotEquals(Character ch1,Character ch2 ) {
+        
+        if(((int) ch1 - (int)ch2) != 0)
+            return true;
+    return false ;
+    
+}
+   
 }
